1988bb18140610b9997eac15cd4c1e970923e72c
Tanmay Hatkar <tanmayhatkar240@gmail.com>
2025-01-01 13:43:27 +0530

    working on readability

--- script.js
+++ script.js
@@ -37,38 +37,157 @@ function dbRender(){
 // card renderer
 function cardRender(evt){
 
-    // set card
-    let card = evt.target.id;
+    if (evt.target.classList.contains("expandable")){
+        
+        // empty tasks and timeline
 
-    // display
-    page.style.display = "block";
-    dashboard.style.display = "none";
 
-    // set head
-    pageHead.innerHTML = card;
+        // set card
+        let card = evt.target.id;
 
-    // set back
-    pageHead.addEventListener("mouseover", changeText)
+        // display
+        page.style.display = "block";
+        dashboard.style.display = "none";
 
-    function changeText(){
-        pageHead.innerHTML = "DASHBOARD ?";
-        pageHead.addEventListener("click", dbRender);
-        pageHead.addEventListener("mouseleave", changeAgain);
-    }
-
-    function changeAgain(){
+        // set head
         pageHead.innerHTML = card;
-        pageHead.removeEventListener("mouseleave", changeAgain);
-    }
 
-    // set adder
-    setAdder();
-    function setAdder(){
-        if(taskbox.children.length >= 4){
-            adder.style.display = "none";
+        // set back
+        pageHead.addEventListener("mouseover", changeText)
+
+        function changeText(){
+            pageHead.innerHTML = "DASHBOARD ?";
+            
+            // ignore class name, used for fun
+            pageHead.classList.add("headChange");
+            pageHead.addEventListener("mouseleave", changeAgain);
+            pageHead.addEventListener("click", emptyAndDb);
         }
-        else{
-            adder.style.display = "flex";
+
+        function changeAgain(){
+            pageHead.innerHTML = card;
+            pageHead.classList.remove("headChange");
+            pageHead.removeEventListener("mouseleave", changeAgain);
+        }
+
+        // empty and dashboard
+        function emptyAndDb(){
+
+            // remove event listners
+            adder.removeEventListener("mouseover", changeAdder);
+            adder.removeEventListener("click", addTask);
+
+            // remove tasks
+            Array.from(taskbox.children).forEach(task => {
+                if(task != adder){
+                    taskbox.removeChild(task);
+                }
+                if(adder.style.display == "none"){
+                    adder.style.display = "flex";
+                }
+            });
+
+            // db renderer
+            dbRender();
+        }
+
+        // set adder
+        setAdder();
+        function setAdder(){
+            if(taskbox.children.length > 4){
+                adder.style.display = "none";
+            }
+            else{
+
+                // display
+                adder.style.display = "flex";
+
+                // adder events
+                adder.addEventListener("mouseover", changeAdder);
+                adder.addEventListener("click", addTask);
+            }
+        }
+
+        // adder interaction
+        function changeAdder(){
+            adder.innerHTML = "ADD TASK ?";
+            adder.classList.add("adderChange");
+            adder.addEventListener("mouseleave", resetAdder);
+
+            function resetAdder(){
+                adder.innerHTML = "+";
+                adder.classList.remove("adderChange");
+                adder.removeEventListener("mouseleave", resetAdder);
+            }
+        }
+
+        // add task
+        function addTask(){
+                
+            let newTask = createTask();
+            taskbox.insertBefore(newTask, adder);
+
+            // focus managemet
+            // manageFocus(adder.previousElementSibling);
+
+            // task events
+            // addTaskEvents(adder.previousElementSibling);
+
+            function createTask(){
+
+                // task
+                let newTask = document.createElement("div");
+                newTask.classList.add("task");
+
+                // title
+                let title = document.createElement("p");
+                title.classList.add("title");
+                title.setAttribute("contenteditable", true);
+
+                // done
+                let done = document.createElement("div");
+                done.classList.add("done");
+                done.innerHTML = `<i class="fa-solid fa-check"></i>`;
+
+                // description
+                let description = document.createElement("div");
+                description.classList.add("description");
+                description.setAttribute("contenteditable", true);
+
+                // append elements to task
+                newTask.append(title, done, description);
+                return newTask;
+            }
+
+            if(taskbox.children.length > 4){
+                adder.style.display = "none";
+            }
+        }
+
+        // focus manager
+        function manageFocus(task){
+
+            // title focus
+            let inFocus = task.firstElementChild;
+
+            while(!inFocus.classList.contains("time"))
+                inFocus.focus();
+
+                // enter event
+                inFocus.addEventListener("keyress", enterEvt);
+
+                function enterEvt(evt){
+                    if(evt.key == "Enter"){
+
+                        evt.preventDefault();
+
+                        inFocus.removeEventListener("keypress", enterEvt);
+                        if(inFocus.classList.contains("title")){
+                            inFocus = inFocus.nextElementSibling.nextElementSibling;
+                        }
+                    }
+                }
+            
         }
     }
 }
